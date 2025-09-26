6000ac2848e2b83f440816a3cd083b8ca1cb1eab
pvhung2112 <phamvanhung21122004@gmail.com>
2025-09-26 22:03:28 +0700

    hehehihi

--- index.js
+++ index.js
@@ -2,33 +2,30 @@ const express = require('express')
 const flash = require('express-flash')
 const cookieParser = require('cookie-parser')
 const session = require('express-session')
+const methodOverride = require('method-override')
+require('dotenv').config()
+
 const app = express()
 
-app.use(cookieParser('sdy89ewyiushdff')); //key tự tạo ra 
-app.use(session({ cookie: { maxAge: 60000 }}));
-app.use(flash());
-require('dotenv').config();
-var methodOverride = require('method-override')
+app.use(cookieParser('sdy89ewyiushdff'))
+app.use(session({ cookie: { maxAge: 60000 }}))
+app.use(flash())
 
-const port = process.env.POST
 const geturladm = require("./config/system.js")
-console.log(__dirname);
 app.set('views', `${__dirname}/views`)
 app.set('view engine', 'pug')
 app.use(express.static(`${__dirname}/public`))
-app.locals.url_Adm = geturladm.url_adm;
+app.locals.url_Adm = geturladm.url_adm
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 app.use(methodOverride('_method'))
+
 const database = require('./config/database')
 const router = require("./routers/admin/index.router")
+
 router(app)
-database.connect();
+database.connect()
 
 
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+module.exports = app
